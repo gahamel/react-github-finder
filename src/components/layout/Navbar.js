8f1ece02355f850aa3c531ef65525641907f7865
gahamel <gaetanhamelpro@gmail.com>
2019-07-22 14:26:26 +0200

    first components and API call

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+const Navbar = ({icon,title}) => {
+    return (
+        <div className='mavbar bg-primary'>
+            <h1>
+            <i className={icon}></i>
+            {title}</h1>
+        </div>
+    )
+}
+
+
+Navbar.defaultProps = {
+    title: 'github finder',
+    icon: 'fab fa-github'
+};
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+};
+
+export default Navbar;
